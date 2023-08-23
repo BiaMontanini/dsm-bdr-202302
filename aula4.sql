f527f5808b48b620aa7a01b24ed667bfced5d91d
Beatriz R. Montanini <montanini249@gmail.com>
2023-08-22 22:46:46 -0300

    aula 4

---
+++ aula4.sql
@@ -0,0 +1,32 @@
+insert into tbl_cliente values (1,'joao da Silva', 'São Paulo','Rua A, 123'),
+				(2,'Maria Santos','Rio de Janeiro','Av B,456'),
+				(3,'Pedro Almeida','Rio de Janeiro','Rua C,789'),
+				(4,'Ana Oliveira','Salvador','Av D,1011'),
+				(5,'Carlos Lima','Brasilia','Rua E,1213');
+
+insert into tbl_titulo values (1, 
+				'Aventuras Urbanas', 
+				'Uma historia emocionante', 
+				'DRAMA'),
+				(2, 
+				'Misterios Antigos', 
+				'Enigmas por resolver', 
+				'COMEDIA'),
+				(3, 
+				'Amor nas Estrelas', 
+				'Um Romance Intergalatico', 
+				'DRAMA'),
+				(4, 
+				'Codigo Enigmatico', 
+				'Segredos ocultos', 
+				'COMEDIA'),
+				(5, 
+				'Historias Perdidas', 
+				'Contos esquecidos', 
+				'DRAMA');
+create table tbl_cliente2 (codigo integer PRIMARY KEY, nome text NOT NULL, cidade text, endereco text);
+insert into tbl_cliente2 values (1,'joao da Silva', 'São Paulo','Rua A, 123'),
+								(2,'Maria Santos','Rio de Janeiro','Av B,456'),
+								(3,'Pedro Almeida','Rio de Janeiro','Rua C,789'),
+								(4,'Ana Oliveira','Salvador','Av D,1011'),
+								(5,'Carlos Lima','Brasilia','Rua E,1213');
